497231f91526d75eaac199670a2cb7372541d76a
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-10-13 17:58:01 +0200

    feat: simple map creation

---
+++ map.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "map.h"
+
+
+t_map   *map_create(int width, int height) {
+    t_map   *map;
+
+    map = malloc(sizeof(t_map));
+    if (map == NULL) {
+        return NULL;
+    }
+
+    map->width = width;
+    map->height = height;
+    map->map = malloc(sizeof(char *) * height);
+    if (map->map == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i < height; i++) {
+        map->map[i] = malloc(sizeof(char) * width);
+        if (map->map[i] == NULL) {
+            return NULL;
+        }
+    }
+
+    return map;
+};
+
+void    map_fill(const t_map *map) {
+    if (map == NULL) return;
+
+    for (int i = 0; i < map->height; i++) {
+        for (int j = 0; j < map->width; j++) {
+            map->map[i][j] = 'X';
+        }
+    }
+}
+
+
+void    map_destroy(t_map *map) {
+    for (int i = 0; i < map->height; i++) {
+        free(map->map[i]);
+    }
+    free(map->map);
+    free(map);
+};
+
+void    map_print(const t_map *map) {
+    for (int i = 0; i < map->height; i++) {
+        for (int j = 0; j < map->width; j++) {
+            printf("%c", map->map[i][j]);
+        }
+        printf("\n");
+    }
+};
