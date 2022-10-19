60ac9f0d2af75d83156248f95099073236cc1185
James <james.bidule@gmail.com>
2022-10-19 23:04:19 +0200

    feat(timer): timer functions

--- timer.h
+++ timer.h
@@ -13,4 +13,15 @@
      void            (*stop)(struct s_timer*);
  }               t_timer;
 
-#endif /* !FOO_H */
+void           initTimer(t_timer *timer);
+void           startTimer(t_timer *timer);
+void           stopTimer(t_timer *timer);
+void           pauseTimer(t_timer *timer);
+void           unpauseTimer(t_timer *timer);
+void           printTimer(t_timer *timer);
+unsigned int   getTimerTicks(t_timer *timer);
+unsigned int   getTimerSeconds(t_timer *timer);
+bool           isTimerStarted(t_timer *timer);
+bool           isTimerPaused(t_timer *timer);
+
+#endif
