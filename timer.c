e571dc220f544355f865d0865131e0b59feabbad
James <james.bidule@gmail.com>
2022-10-20 17:00:37 +0200

    feat(timer): clean timer

--- timer.c
+++ timer.c
@@ -1,58 +1,51 @@
 #include <SDL_timer.h>
 #include "timer.h"
 
-void    initTimer(t_timer *timer) {
-    timer->paused = false;
-    timer->started = false;
-    timer->startTicks = 0;
-    timer->pausedTicks = 0;
-}
-
 void    startTimer(t_timer *timer) {
-    timer->started = true;
-    timer->paused = false;
     timer->startTicks = SDL_GetTicks64();
-    timer->pausedTicks = 0;
 }
 
 void    stopTimer(t_timer *timer) {
-    timer->started = false;
     timer->paused = false;
     timer->startTicks = 0;
     timer->pausedTicks = 0;
 }
 
 void    pauseTimer(t_timer *timer) {
-    if (timer->started && !timer->paused) {
+    if (!isTimerPaused(timer) && !isTimerFinished(timer)) {
         timer->paused = true;
-        timer->pausedTicks = SDL_GetTicks64() - timer->startTicks;
+        timer->pausedTicks = getTimerDuration(timer);
     }
 }
 
 void    unpauseTimer(t_timer *timer) {
-    if (timer->started && timer->paused) {
+    if (isTimerPaused(timer) && !isTimerFinished(timer)) {
         timer->paused = false;
-        timer->startTicks = SDL_GetTicks64() - timer->pausedTicks;
+        timer->startTicks = getTimerDuration(timer);
         timer->pausedTicks = 0;
     }
 }
 
-// Print the timer in the console [DEBUG ONLY]
+// Print timer info in console
 void    printTimer(t_timer *timer) {
-    printf("Timer Milliseconds: %d\n", getTimerTicks(timer));
-    printf("Timer seconds: %d\n", getTimerSeconds(timer));
+    printf("Timer Milliseconds: %d, Timer Seconds: %d, Duration: %d, Timer Paused: %d, Timer Finished: %d\n",
+           getTimerTicks(timer), getTimerSeconds(timer),getDuration(timer), isTimerPaused(timer), isTimerFinished(timer));
+}
+
+// Get duration of the timer
+unsigned int    getTimerDuration(t_timer *timer) {
+    if (isTimerPaused(timer)) {
+        return SDL_GetTicks64() - timer->startTicks;
+    }
+    return SDL_GetTicks64() - timer->pausedTicks;
 }
 
 // Return the number of ticks since the timer started in milliseconds (s)
 unsigned int    getTimerTicks(t_timer *timer) {
-    unsigned int time = 0;
-    if (timer->started) {
-        if (timer->paused)
-            time = timer->pausedTicks;
-        else
-            time = SDL_GetTicks64() - timer->startTicks;
+    if (isTimerPaused(timer)) {
+        return timer->pausedTicks;
     }
-    return time;
+    return SDL_GetTicks64() - timer->startTicks;
 }
 
 // transform milliseconds ticks to seconds
@@ -60,11 +53,20 @@ unsigned int   getTimerSeconds(t_timer *timer) {
     return getTimerTicks(timer) / 1000;
 }
 
-bool    isTimerStarted(t_timer *timer) {
-    return timer->started;
+// return duration max of the timer
+unsigned int    getDuration(t_timer *timer) {
+    return timer->duration;
+}
+
+// Return the number of seconds left before the timer is finished
+unsigned int   getTimeLeft(t_timer *timer) {
+    return getDuration(timer) - getTimerSeconds(timer);
 }
 
 bool    isTimerPaused(t_timer *timer) {
     return timer->paused;
 }
 
+bool    isTimerFinished(t_timer *timer) {
+    return getTimerSeconds(timer) >= getDuration(timer);
+}
--- timer.h
+++ timer.h
@@ -7,21 +7,23 @@
  {
      unsigned int    startTicks;
      unsigned int    pausedTicks;
+     unsigned int   duration;
      bool            paused;
-     bool            started;
-     void            (*start)(struct s_timer*);
+     void            (*start)(struct s_timer *timer);
      void            (*stop)(struct s_timer*);
  }               t_timer;
 
-void           initTimer(t_timer *timer);
-void           startTimer(t_timer *timer);
-void           stopTimer(t_timer *timer);
-void           pauseTimer(t_timer *timer);
-void           unpauseTimer(t_timer *timer);
-void           printTimer(t_timer *timer);
-unsigned int   getTimerTicks(t_timer *timer);
-unsigned int   getTimerSeconds(t_timer *timer);
-bool           isTimerStarted(t_timer *timer);
-bool           isTimerPaused(t_timer *timer);
+ void           startTimer(t_timer *timer);
+ void           stopTimer(t_timer *timer);
+ void           pauseTimer(t_timer *timer);
+ void           unpauseTimer(t_timer *timer);
+ void           printTimer(t_timer *timer);
+ unsigned int   getTimerTicks(t_timer *timer);
+ unsigned int   getTimerSeconds(t_timer *timer);
+ unsigned int   getTimerDuration(t_timer *timer);
+ unsigned int   getTimeLeft(t_timer *timer);
+ unsigned int   getDuration(t_timer *timer);
+ bool           isTimerPaused(t_timer *timer);
+ bool           isTimerFinished(t_timer *timer);
 
 #endif
