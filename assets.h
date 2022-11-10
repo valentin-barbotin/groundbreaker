00e08a845d9238f17205436075cd7fcc528ed83c
James <james.bidule@gmail.com>
2022-11-10 15:19:56 +0100

    feat(inventory): added items textures

--- assets.h
+++ assets.h
@@ -6,8 +6,21 @@
  #define TEX_PLAYER             "./assets/bomber.png"
  #define TEX_WALL               "./assets/wall.png"
  #define TEX_DIRT               "./assets/dirt.png"
+ // Items
+ #define TEX_BOMB                    "./assets/items/bomb.png"
+ #define TEX_BOMB_UP                 "./assets/items/bomb_up.png"
+ #define TEX_BOMB_DOWN               "./assets/items/bomb_down.png"
+ #define TEX_YELLOW_FLAME            "./assets/items/yellow_flame.png"
+ #define TEX_BLUE_FLAME              "./assets/items/blue_flame.png"
+ #define TEX_RED_FLAME               "./assets/items/red_flame.png"
+ #define TEX_PASS_THROUGH            "./assets/items/pass_through.png"
+ #define TEX_BOMB_KICK               "./assets/items/bomb_kick.png"
+ #define TEX_INVINCIBILITY           "./assets/items/invincibility.png"
+ #define TEX_HEART                   "./assets/items/heart.png"
+ #define TEX_LIFE                    "./assets/items/life.png"
 
- #define SOUND_UNBREAKABLE_WALL "./assets/sound/unbreakable_wall.ogg"
+
+#define SOUND_UNBREAKABLE_WALL "./assets/sound/unbreakable_wall.ogg"
  #define SOUND_MUSIC_MAIN       "./assets/sound/main_music.ogg"
  #define SOUND_WALK             "./assets/sound/walk.ogg"
  #define SOUND_WALL             "./assets/sound/wall.ogg"
