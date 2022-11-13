d2ebaf5c126bc4f42637e5e1e13fc49da910a488
James <james.bidule@gmail.com>
2022-11-13 16:46:22 +0100

    feat(tchat): user can tchat while is playing

--- tchat.c
+++ tchat.c
@@ -4,8 +4,8 @@
 #include "font.h"
 #include "dialog.h"
 
-char   **messages[];
-
+char   **g_messages = NULL;
+size_t  g_messages_nb = 0;
 
 // strdup
 // TODO : player in green and other in white
@@ -33,7 +33,6 @@ void    askTchatMessage() {
     t_dialog  *dialog;
 
     dialog = createEditBox("Send a message :", 20, (SDL_Color){255, 255, 255, 255}, (SDL_Color){0, 0, 0, 255});
-
     memset(dialog->edit, 0, sizeof(dialog->edit));
     dialog->callback = askTchatMessageCallback;
 }
@@ -43,6 +42,7 @@ void    askTchatMessageCallback(const char *msg) {
     t_player *player;
 
     game = getGame();
+    player = getPlayer();
 
     if (msg) {
         putMessageInTchat(player, msg);
@@ -51,5 +51,64 @@ void    askTchatMessageCallback(const char *msg) {
 }
 
 void    putMessageInTchat(t_player *player, const char *msg) {
-    printf("%s", msg);
+    char *buffer;
+    if(g_messages == NULL) {
+        g_messages = malloc(sizeof(char*));
+        g_messages_nb++;
+    } else {
+        g_messages = realloc(g_messages , sizeof(char*) * ++g_messages_nb);
+    }
+    buffer = malloc(sizeof(char) * (strlen(msg) + 1 + strlen(player->name) + 1));
+    sprintf(buffer, "%s: %s", player->name, msg);
+    g_messages[g_messages_nb - 1] = buffer;
+    buffer = NULL;
+    free(buffer);
+
 }
+
+
+void    drawTchatMessages() {
+    char *buffer;
+    int i, limit;
+
+    SDL_Rect rect;
+    SDL_Color color = {255, 255, 255, 255};
+
+    rect.x = 0;
+    rect.y = 0;
+
+    if(g_messages_nb == 0) {
+        return;
+    }
+
+    i = g_messages_nb;
+    limit = g_messages_nb - 5;
+
+
+    // TODO : get the last 5 messages
+    while((i &&  g_messages[i - 1] != NULL && i > limit)) {
+        // TODO : retrieve new technic to get last...
+        buffer = g_messages[i-1];
+        drawText(&color, rect.x + 10, rect.y + 10, buffer, false, 0);
+        rect.y += 20;
+        i--;
+    }
+
+}
+
+/*
+t_game *game;
+SDL_Rect rect;
+SDL_Color color = {255, 255, 255, 255};
+game = getGame();
+
+
+size_t nbMessages = getTchatMessagesNb();
+char **messages = getTchatMessages();
+pickColor(&color);
+
+for (size_t i = 0; i < nbMessages; i++) {
+    drawText(&color, 0, gameConfig->video.height * 0.4 + i * 20, messages[i], false, 0);
+}
+*/
+
